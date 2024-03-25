b06263b12094335eda528d1a040b80de11674c6b
shahla.g <shahla.guliyeva@cybernet.az>
2024-03-26 01:13:43 +0400

    add events to nav

--- Travelia/assets/js/script.js
+++ Travelia/assets/js/script.js
@@ -3,3 +3,26 @@ window.addEventListener("load", ()=>{
     preloader.classList.add("remove");
 });
 
+
+// add event on multiple elements 
+
+const addEventOnElement = function (elements ,eventType, callback)
+{
+    for(let i=0, len = elements.length; i <len ; i++)
+    {
+        elements[i].addEventListener(eventType,callback);
+    }
+}
+
+const navbar = document.querySelector("[data-navbar]"); 
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay =document.querySelector("[data-overlay]");
+
+const toggleNav = function() 
+{
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElement(navTogglers, "click",toggleNav);
